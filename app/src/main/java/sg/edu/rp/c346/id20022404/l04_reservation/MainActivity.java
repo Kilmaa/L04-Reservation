f5a72dee0c0a5e559616ce051b97e76a1dc1bec9
Kilmaa <20022404@myrp.edu.sg>
2021-05-12 00:48:33 +0800

    Enhancement: Scenario  1

--- app/src/main/java/sg/edu/rp/c346/id20022404/l04_reservation/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20022404/l04_reservation/MainActivity.java
@@ -3,6 +3,7 @@ package sg.edu.rp.c346.id20022404.l04_reservation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     DatePicker dp;
@@ -41,25 +43,42 @@ public class MainActivity extends AppCompatActivity {
         tp.setCurrentHour(19);
         tp.setCurrentMinute(30);
         dp.updateDate(2020, 5,1);
+        rGroup.check(R.id.nSmoking);
 
+        tp.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                if(hourOfDay < 8) {
+                    tp.setCurrentHour(8);
+                }
+                else if(hourOfDay > 20) {
+                    tp.setCurrentHour(20);
+                    tp.setCurrentMinute(59);
+                }
+            }
+        });
 
         btnConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String areaOpt = "";
-                if (rGroup.getCheckedRadioButtonId() == R.id.smoking) {
-                    areaOpt = "Smoking Area";
+                if (!TextUtils.isEmpty(name.getText().toString()) || !TextUtils.isEmpty(phone.getText().toString()) || !TextUtils.isEmpty(gSize.getText().toString())) {
+                    String areaOpt = "";
+                    if (rGroup.getCheckedRadioButtonId() == R.id.smoking) {
+                        areaOpt = "Smoking Area";
+                    } else {
+                        areaOpt = "Non-smoking Area";
+                    }
+
+                    String timeis = tp.getCurrentHour() + ":" + String.format("%02d", tp.getCurrentMinute());
+                    String dateis = dp.getDayOfMonth() + "/" + (dp.getMonth()+1) + "/" + dp.getYear();
+                    tvName.setText("Reservee name: " + name.getText().toString());
+                    tvPhone.setText("Mobile number: " + phone.getText().toString());
+                    tvResult.setText("Reservation for a group of " + gSize.getText().toString() + " on " + dateis + " at " + timeis
+                            + " in a " + areaOpt);
+
                 } else {
-                    areaOpt = "Non-smoking Area";
+                    Toast.makeText(MainActivity.this, "Error: Empty field is not allowed!", Toast.LENGTH_LONG).show();
                 }
-
-                String timeis = tp.getCurrentHour() + ":" + String.format("%02d", tp.getCurrentMinute());
-                String dateis = dp.getDayOfMonth() + "/" + (dp.getMonth()+1) + "/" + dp.getYear();
-                tvName.setText("Reservee name: " + name.getText().toString());
-                tvPhone.setText("Mobile number: " + phone.getText().toString());
-                tvResult.setText("Reservation for a group of " + gSize.getText().toString() + " on " + dateis + " at " + timeis
-                        + " in a " + areaOpt);
-
             }
         });
 
@@ -69,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
                 tp.setCurrentHour(19);
                 tp.setCurrentMinute(30);
                 dp.updateDate(2020, 5,1);
-                rGroup.clearCheck();
+                rGroup.check(R.id.nSmoking);
                 name.setText(blank);
                 phone.setText(blank);
                 gSize.setText(blank);
