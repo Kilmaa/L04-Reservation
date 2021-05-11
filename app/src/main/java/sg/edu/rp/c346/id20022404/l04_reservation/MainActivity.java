4cad4878680a530b9f2078f5478391f46462cd23
Kilmaa <20022404@myrp.edu.sg>
2021-05-12 00:00:24 +0800

    Final Answer

--- app/src/main/java/sg/edu/rp/c346/id20022404/l04_reservation/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20022404/l04_reservation/MainActivity.java
@@ -3,19 +3,22 @@ package sg.edu.rp.c346.id20022404.l04_reservation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
     DatePicker dp;
     TimePicker tp;
     Button btnConfirm, btnReset;
-    TextView tvDisplay;
+    TextView tvName, tvPhone,tvResult;
     EditText name, phone, gSize;
+    RadioGroup rGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +27,55 @@ public class MainActivity extends AppCompatActivity {
 
         dp = findViewById(R.id.datePicker);
         tp = findViewById(R.id.timePicker);
-        tvDisplay = findViewById(R.id.textViewDisplay);
+        tvName = findViewById(R.id.displayName);
+        tvPhone = findViewById(R.id.displayPhone);
+        tvResult = findViewById(R.id.displayResult);
         btnConfirm = findViewById(R.id.buttonConfirm);
         btnReset = findViewById(R.id.buttonReset);
         name = findViewById(R.id.editTextName);
         phone = findViewById(R.id.editTextPhone);
         gSize = findViewById(R.id.editTextGroup);
+        rGroup = findViewById(R.id.smokingOpt);
+        String blank = "";
+
+        tp.setCurrentHour(19);
+        tp.setCurrentMinute(30);
+        dp.updateDate(2020, 5,1);
+
 
         btnConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String areaOpt = "";
+                if (rGroup.getCheckedRadioButtonId() == R.id.smoking) {
+                    areaOpt = "Smoking Area";
+                } else {
+                    areaOpt = "Non-smoking Area";
+                }
+
+                String timeis = tp.getCurrentHour() + ":" + String.format("%02d", tp.getCurrentMinute());
+                String dateis = dp.getDayOfMonth() + "/" + (dp.getMonth()+1) + "/" + dp.getYear();
+                tvName.setText("Reservee name: " + name.getText().toString());
+                tvPhone.setText("Mobile number: " + phone.getText().toString());
+                tvResult.setText("Reservation for a group of " + gSize.getText().toString() + " on " + dateis + " at " + timeis
+                        + " in a " + areaOpt);
+
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tp.setCurrentHour(19);
+                tp.setCurrentMinute(30);
+                dp.updateDate(2020, 5,1);
+                rGroup.clearCheck();
+                name.setText(blank);
+                phone.setText(blank);
+                gSize.setText(blank);
+                tvName.setText(blank);
+                tvPhone.setText(blank);
+                tvResult.setText(blank);
 
             }
         });
